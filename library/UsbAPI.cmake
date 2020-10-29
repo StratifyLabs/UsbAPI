e4e454c629a71336d365b5651e8811916fb89a50
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-10-28 21:19:51 -0600

    v4 updates

--- library/UsbAPI.cmake
+++ library/UsbAPI.cmake
@@ -1,4 +1,5 @@
 
-include(targets/JsonAPI)
-
-sos_sdk_include_target(UsbAPI "${STRATIFYAPI_CONFIG_LIST}")
+if(NOT DEFINED IS_SDK)
+	include(JsonAPI)
+	sos_sdk_include_target(UsbAPI "${STRATIFYAPI_CONFIG_LIST}")
+endif()
